142b51cca109591a9291335f5c9955ac77e3e36c
Mohit Choudhary <mohit.choudhary@algolia.com>
2023-05-17 15:28:13 +0530

    Updated Queue Archieve code

---
+++ Model/ResourceModel/QueueArchive/Grid/Collection.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace Algolia\AlgoliaSearch\Model\ResourceModel\QueueArchive\Grid;
+
+use Algolia\AlgoliaSearch\Model\ResourceModel\QueueArchive\Collection as QueueArchiveCollection;
+use Magento\Framework\Api\ExtensibleDataInterface;
+use Magento\Framework\Api\Search\AggregationInterface;
+use Magento\Framework\Api\Search\SearchResultInterface;
+use Magento\Framework\Api\SearchCriteriaInterface;
+use Magento\Framework\Data\Collection\Db\FetchStrategyInterface;
+use Magento\Framework\Data\Collection\EntityFactoryInterface;
+use Magento\Framework\Event\ManagerInterface;
+use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
+use Psr\Log\LoggerInterface;
+
+class Collection extends QueueArchiveCollection implements SearchResultInterface
+{
+    /** @var AggregationInterface */
+    protected $aggregations;
+
+    /**
+     * @param EntityFactoryInterface $entityFactory
+     * @param LoggerInterface $logger
+     * @param FetchStrategyInterface $fetchStrategy
+     * @param ManagerInterface $eventManager
+     * @param mixed|null $mainTable
+     * @param AbstractDb $eventPrefix
+     * @param mixed $eventObject
+     * @param mixed $resourceModel
+     * @param string $model
+     * @param null $connection
+     * @param AbstractDb|null $resource
+     *
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     */
+    public function __construct(
+        EntityFactoryInterface $entityFactory,
+        LoggerInterface $logger,
+        FetchStrategyInterface $fetchStrategy,
+        ManagerInterface $eventManager,
+        $mainTable,
+        $eventPrefix,
+        $eventObject,
+        $resourceModel,
+        $model = 'Magento\Framework\View\Element\UiComponent\DataProvider\Document',
+        $connection = null,
+        AbstractDb $resource = null
+    ) {
+        parent::__construct(
+            $entityFactory,
+            $logger,
+            $fetchStrategy,
+            $eventManager,
+            $connection,
+            $resource
+        );
+        $this->_eventPrefix = $eventPrefix;
+        $this->_eventObject = $eventObject;
+        $this->_init($model, $resourceModel);
+        $this->setMainTable($mainTable);
+    }
+
+    /** @return AggregationInterface */
+    public function getAggregations(): AggregationInterface
+    {
+        return $this->aggregations;
+    }
+
+    /**
+     * @param AggregationInterface $aggregations
+     *
+     * @return void
+     */
+    public function setAggregations($aggregations)
+    {
+        $this->aggregations = $aggregations;
+    }
+
+    /** @return SearchCriteriaInterface|null */
+    public function getSearchCriteria()
+    {
+        return null;
+    }
+
+    /**
+     * @param SearchCriteriaInterface|null $searchCriteria
+     *
+     * @return $this
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function setSearchCriteria(SearchCriteriaInterface $searchCriteria = null)
+    {
+        return $this;
+    }
+
+    /** @return int */
+    public function getTotalCount()
+    {
+        return $this->getSize();
+    }
+
+    /**
+     * @param int $totalCount
+     *
+     * @return $this
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function setTotalCount($totalCount)
+    {
+        return $this;
+    }
+
+    /**
+     * @param ExtensibleDataInterface[] $items
+     *
+     * @return $this
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function setItems(array $items = null)
+    {
+        return $this;
+    }
+}
